8adfdf4e11eca20eabc0b53cef502ad2f48f1660
Jack Davidson <jakd36@gmail.com>
2019-12-04 19:07:58 +0000

    Saving first attempt

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -7,18 +8,8 @@ int RunIntCode(int* intCode);
 
 int main()
 {
-    // ops 
-    // 1 Adds
-    // 2 multiplies
-    // 99 end
-
-    // Pos 0 operations
-    // Pos 1 input 1 position
-    // Pos 2 input 1 position
-    // Pos 3 output position
-
     vector<int> intCode;
-
+   
     char filepath[] = "Day2/input.txt";
     FILE* file = fopen(filepath,"r");
     int val;
@@ -29,9 +20,157 @@ int main()
         fscanf(file,",");
     }    
 
+    // Build new opcode program
+    set<int> potentialUnknowns;
+    potentialUnknowns.insert(1);
+    potentialUnknowns.insert(2);
+    // Collect old ops
+    int i = 0;
+    while(intCode[i] != 99)
+    {
+        potentialUnknowns.insert(intCode[i+3]);
+        i+=4;
+    }
+
+    vector<int> unknownOps;
+    potentialUnknowns.erase(0);
+    vector<int> newOps;
+    for(int n = i-4; n >= 0; n-=4)
+    {
+        // Get the reverse op + -> - * -> /
+        newOps.push_back(intCode[n] + 2); // Convert + to - and * to /
+        if(intCode[n] == 1)
+        {
+            printf("%d + %d -> %d\t",intCode[n+1],intCode[n+2],intCode[n+3]);
+        }
+        else if(intCode[n] == 2)
+        {
+            printf("%d * %d -> %d\t",intCode[n+1],intCode[n+2],intCode[n+3]);    
+        }
     
-    int output = RunIntCode(intCode.data());
-    printf("%d\n",output);
+        int unknowns = 0;
+        bool firstKnown = potentialUnknowns.find(intCode[n+1]) == potentialUnknowns.end();
+        bool secondKnown = potentialUnknowns.find(intCode[n+2]) == potentialUnknowns.end();
+        bool resultKnown = potentialUnknowns.find(intCode[n+3]) == potentialUnknowns.end();
+        
+        if(firstKnown && !secondKnown && resultKnown)
+        {
+            newOps.push_back(intCode[n+3]);
+            newOps.push_back(intCode[n+1]);
+            newOps.push_back(intCode[n+2]);
+            potentialUnknowns.erase(intCode[n+3]);
+            if(intCode[n] + 2 ==3)
+            {
+                printf("%d - %d -> %d\n",intCode[n+3],intCode[n+1],intCode[n+2]);
+            }
+            else if(intCode[n] + 2 == 4)
+            {
+                printf("%d / %d -> %d\n",intCode[n+3],intCode[n+1],intCode[n+2]);    
+            }
+            // printf("Erase %d\n",intCode[n+2]);
+        }
+        else if(!firstKnown && secondKnown && resultKnown)
+        {
+            newOps.push_back(intCode[n+3]);
+            newOps.push_back(intCode[n+2]);
+            newOps.push_back(intCode[n+1]);
+            potentialUnknowns.erase(intCode[n+3]);
+            if(intCode[n] + 2 == 3)
+            {
+                printf("%d - %d -> %d\n",intCode[n+3],intCode[n+2],intCode[n+1]);
+            }
+            else if(intCode[n] + 2 == 4)
+            {
+                printf("%d / %d -> %d\n",intCode[n+3],intCode[n+2],intCode[n+1]);    
+            }
+            // printf("Erase %d\n",intCode[n+1]);
+        }
+        else if( (!firstKnown && !secondKnown))
+        {
+            printf("\n");
+            printf("Two Unknowns %d %d\n",intCode[n+1],intCode[n+2]);
+            newOps.pop_back();
+            // unknownOps.push_back(intCode[n]);
+            // unknownOps.push_back(intCode[n+1]);
+            // unknownOps.push_back(intCode[n+2]);
+            // unknownOps.push_back(intCode[n+3]);
+        }
+        else if( (!firstKnown && !resultKnown) )
+        {
+            printf("\n");
+            printf("Two Unknowns %d %d\n",intCode[n+1],intCode[n+3]);
+            newOps.pop_back();
+            // unknownOps.push_back(intCode[n]);
+            // unknownOps.push_back(intCode[n+1]);
+            // unknownOps.push_back(intCode[n+2]);
+            // unknownOps.push_back(intCode[n+3]);
+        }
+        else if(  (!secondKnown && !resultKnown))
+        {
+            printf("\n");
+            printf("Two Unknowns %d %d\n",intCode[n+2],intCode[n+3]);
+            newOps.pop_back();
+            // unknownOps.push_back(intCode[n]);
+            // unknownOps.push_back(intCode[n+1]);
+            // unknownOps.push_back(intCode[n+2]);
+            // unknownOps.push_back(intCode[n+3]);
+        }
+        else
+        {
+            potentialUnknowns.erase(intCode[n+3]);
+            printf("\n");
+            printf("Help\n");
+        }
+    }
+
+    for(set<int>::iterator it = potentialUnknowns.begin(); it != potentialUnknowns.end(); ++it)
+    {
+        printf("%d ",*it);
+    }printf("\n");
+
+    for(int n = 0; n < newOps.size(); n++)
+    {
+        printf("%d ", newOps[n]);
+    }
+    printf("\n");
+
+    switch(newOps[0])
+    {
+        case 3:
+            
+            intCode[newOps[3]] = 19690720 - intCode[newOps[2]];
+            printf("%d - %d -> %d\t%d - %d = %d\n",newOps[1],newOps[2],newOps[3],19690720,intCode[newOps[2]],intCode[newOps[3]]);
+            break;
+        case 4:
+            intCode[newOps[3]] = 19690720 / intCode[newOps[2]];
+            printf("%d / %d -> %d\t%d / %d = %d\n",newOps[1],newOps[2],newOps[3],19690720,intCode[newOps[2]],intCode[newOps[3]]);
+            break;
+    }
+    for(int n = 4; n < newOps.size(); n+=4)
+    {
+        switch(newOps[n])
+        {
+            case 3:
+                intCode[newOps[n+3]] = intCode[newOps[n+1]] - intCode[newOps[n+2]];
+                printf("%d - %d -> %d\t%d - %d = %d\n",newOps[n+1],newOps[n+2],newOps[n+3],intCode[newOps[n+1]],intCode[newOps[n+2]],intCode[newOps[n+3]]);
+                break;
+            case 4:
+                intCode[newOps[n+3]] = intCode[newOps[n+1]] / intCode[newOps[n+2]];
+                printf("%d / %d -> %d\t%d / %d = %d\n",newOps[n+1],newOps[n+2],newOps[n+3],intCode[newOps[n+1]],intCode[newOps[n+2]],intCode[newOps[n+3]]);
+                break;
+        }
+    }
+
+    for(int n = 0; n < unknownOps.size(); n++)
+    {
+        printf("%d ", unknownOps[n]);
+    }
+    printf("\n");
+    
+    printf("%d %d",intCode[1],intCode[2]);
+    
+    // int output = RunIntCode(intCode.data());
+    // printf("%d\n",output);
     return 0;
 }
 
@@ -47,6 +186,12 @@ int RunIntCode(int* intCode)
             break;
         case 2:
             intCode[intCode[i+3]] = intCode[intCode[i + 1]] * intCode[intCode[i + 2]];
+            break;
+        case 3:
+            intCode[intCode[i+3]] = intCode[intCode[i + 1]] - intCode[intCode[i + 2]];
+            break;
+        case 4:
+            intCode[intCode[i+3]] = intCode[intCode[i + 1]] / intCode[intCode[i + 2]];
             break;
         }
         i+=4;
