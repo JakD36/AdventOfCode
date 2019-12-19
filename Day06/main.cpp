014e202cc6868767e587c707fca9378392f6276e
Jack Davidson <jakd36@gmail.com>
2019-12-19 00:44:57 +0000

    Cleaned up solution to day06, removed debug file writes

--- Day06/main.cpp
+++ Day06/main.cpp
@@ -13,12 +13,12 @@ int main()
     
     unordered_map<string,string> orbits;
    
+    // Gather inputs
     char satelite[4];
     int i = 0;
     char mass[4];
     while(fscanf(file,"%3c)%3c\n",mass,satelite) != EOF)
     {                    
-        // printf("%3s orbits %3s",satelite,mass);
         orbits.insert(make_pair<string,string>(satelite,mass));
     }
     fclose(file);
@@ -26,75 +26,52 @@ int main()
     // Part 1
     unordered_map<string,string>::iterator iter;
     int total = 0;
-    // FILE* out = fopen("Day06/debug.txt","a");
     for(iter = orbits.begin(); iter != orbits.end(); ++iter)
     {
         string recursiveMass = "";
         unordered_map<string,string>::iterator tmp = iter;
-        // fprintf(out,"%s\t",tmp->first.c_str());
-        // fprintf(out,"%s\t",tmp->second.c_str());
         ++total;
         while(tmp != orbits.end() && tmp->second != "COM")
         {
             ++total;
             tmp = orbits.find(tmp->second);
-            // if(tmp != orbits.end())
-            //     fprintf(out,"%s\t",tmp->second.c_str());
         }
-        // fprintf(out,"\n");
     }
-    // fclose(out);
-    printf("%d\n",total);
+    printf("Total Orbits = %d\n",total);
 
     // Part 2
-    unordered_map<string,string>::iterator uIter = orbits.find("YOU");
-    unordered_map<string,string>::iterator sanIter = orbits.find("SAN");
-    unordered_map<string,string>::iterator tmp = uIter;
-
+    iter = orbits.find("YOU");
     vector<string> visitedOrbits;
-    while(tmp != orbits.end() && tmp->second != "COM")
+    while(iter != orbits.end() && iter->second != "COM")
     {       
-        visitedOrbits.push_back(tmp->second); 
-        tmp = orbits.find(tmp->second);     
+        visitedOrbits.push_back(iter->second); 
+        iter = orbits.find(iter->second);     
     }
-    visitedOrbits.push_back(tmp->second);
+    visitedOrbits.push_back(iter->second);
     
-    vector<string>::iterator printIter;
-    FILE* debug = fopen("Day06/debug.txt","a");
-    for(printIter = visitedOrbits.begin(); printIter != visitedOrbits.end(); ++printIter)
-    {
-        fprintf(debug,"%s\t",(*printIter).c_str());
-    }
-    fprintf(debug,"\n");
     
-
-    tmp = sanIter;
-    vector<string>::iterator setIter;
+    iter = orbits.find("SAN");
+    vector<string>::iterator searchIter;
     int count = 0;
-    bool answerFound = false;
-    fprintf(debug,"%s\t",tmp->first.c_str());
-    while(tmp != orbits.end() && tmp->second != "COM")
+    while(iter != orbits.end() && iter->second != "COM")
     {
-        
-        setIter = find(visitedOrbits.begin(),visitedOrbits.end(),tmp->second);
-        fprintf(debug,"%s\t",tmp->second.c_str());
-        if(setIter != visitedOrbits.end() && !answerFound)
+        searchIter = find(visitedOrbits.begin(),visitedOrbits.end(),iter->second);
+        if(searchIter != visitedOrbits.end())
         {
-            printf("Common node %s\n",tmp->second.c_str());
-            printf("YOU -> common %d\n",std::distance(visitedOrbits.begin(), setIter));
-            printf("SAN -> common %d\n",count);
-
-            int answer = (int)(std::distance(visitedOrbits.begin(), setIter)) + count;
-            printf("Answer = %d\n",answer);
-            answerFound = true;
-            // break;
+            break;
         }
-        tmp = orbits.find(tmp->second);
+        iter = orbits.find(iter->second);
         ++count;
     }
-    fprintf(debug,"%s",tmp->second.c_str());
-    fprintf(debug,"\n");
-    fclose(debug);
+
+    int distanceToCommon = (int)std::distance(visitedOrbits.begin(), searchIter);
+
+    printf("Common node %s\n",iter->second.c_str());
+    printf("YOU -> common %d\n",distanceToCommon);
+    printf("SAN -> common %d\n",count);
+
+    int answer = distanceToCommon + count;
+    printf("Part 2 Answer = %d\n",answer);
 
     return 0;
 }
