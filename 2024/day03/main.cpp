734e1fb3ce8c3249e5722c025257778fd430c445
Jack Davidson <jakd36@gmail.com>
2024-12-03 21:52:12 +0000

    completed day 3 part 2 just required some minor changes to original solution

--- 2024/day03/main.cpp
+++ 2024/day03/main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 
 void Part1(const char* filepath);
+void Part2(const char* filepath);
 
 enum class PatternMatchMode
 {
@@ -11,13 +12,14 @@ enum class PatternMatchMode
     ERROR
 };
 
-int ExtractIntegersFromNextPattern(const char* buffer, const int bufferSize,const char*& endPtr, const char* pattern, int* values, const int maxValues);
+bool TryMatchPattern(const char* buffer, const char* bufferEnd,const char*& endPtr, const char* pattern, int* values, const int maxValues);
 
 int main() {
     const char* filepath = "input.txt";
     // const char* filepath = "testInput.txt";
 
     Part1(filepath);
+    Part2(filepath);
     return 0;
 }
 
@@ -39,13 +41,18 @@ void Part1(const char* filepath)
 
     while (fgets(buffer, bufferSize, f) != nullptr)
     {
+        size_t len = strlen(buffer);
+        const char* bufferEnd = buffer + len;
         const char* endPtr;
         int values[2];
         const char* ptr = buffer;
-        while (ExtractIntegersFromNextPattern(ptr, bufferSize, endPtr, "mul(%d,%d)", values, 2) > 0)
+        while (*ptr != '\0' && ptr < bufferEnd)
         {
-            sum += values[0] * values[1];
-            ptr = endPtr;
+            if (TryMatchPattern(ptr, bufferEnd, endPtr, "mul(%d,%d)", values, 2))
+            {
+                sum += values[0] * values[1];
+            }
+            ptr = endPtr + 1;
         }
     }
     fclose(f);
@@ -53,6 +60,57 @@ void Part1(const char* filepath)
     uint64_t elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
     printf_s("Part 1 %d in %llu us\n", sum, elapsed);
 }
+
+void Part2(const char* filepath)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    FILE* f;
+    if (fopen_s(&f, filepath, "r") != 0)
+    {
+        printf_s("Failed to read filepath: %s", filepath);
+        return;
+    };
+
+    const int bufferSize = 2048 * 5;
+    char buffer[bufferSize];
+
+    int sum = 0;
+
+    bool enabled = true;
+
+    while (fgets(buffer, bufferSize, f) != nullptr)
+    {
+        size_t len = strlen(buffer);
+        const char* bufferEnd = buffer + len;
+
+        const char* endPtr;
+        int values[2];
+        const char* ptr = buffer;
+        while (*ptr != '\0' && ptr < bufferEnd)
+        {
+            if (TryMatchPattern(ptr, bufferEnd, endPtr, "mul(%d,%d)", values, 2))
+            {
+                if (enabled)
+                    sum += values[0] * values[1];
+            }
+            else if (TryMatchPattern(ptr, bufferEnd, endPtr, "do()", values, 2))
+            {
+                enabled = true;
+            }
+            else if (TryMatchPattern(ptr, bufferEnd, endPtr, "don't()", values, 2))
+            {
+                enabled = false;
+            }
+            ptr = endPtr + 1;
+        }
+    }
+    fclose(f);
+
+    uint64_t elapsed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    printf_s("Part 1 %d in %llu us\n", sum, elapsed);
+}
+
 
 PatternMatchMode ExtractMode(const char* pattern, const int patternSize, int& index)
 {
@@ -79,7 +137,7 @@ PatternMatchMode ExtractMode(const char* pattern, const int patternSize, int& in
     return PatternMatchMode::ERROR;
 }
 
-int ExtractIntegersFromNextPattern(const char* buffer, const int bufferSize,const char*& endPtr, const char* pattern, int* values, const int maxValues)
+bool TryMatchPattern(const char* buffer, const char* bufferEnd,const char*& endPtr, const char* pattern, int* values, const int maxValues)
 {
     const int patternSize = strlen(pattern);
     int patternIndex = 0;
@@ -87,6 +145,7 @@ int ExtractIntegersFromNextPattern(const char* buffer, const int bufferSize,cons
 
     int valuesSaved = 0;
 
+    int bufferSize = bufferEnd - buffer;
     for (int i = 0; i < bufferSize && buffer[i] != '\0'; ++i)
     {
         switch (mode)
@@ -97,16 +156,16 @@ int ExtractIntegersFromNextPattern(const char* buffer, const int bufferSize,cons
                     ++patternIndex;
                     if (patternIndex == patternSize)
                     {
-                        endPtr = &buffer[i + 1];
-                        return valuesSaved;
+                        endPtr = &buffer[i];
+                        return true;
                     }
 
                     mode = ExtractMode(pattern, patternSize, patternIndex);
                 }
                 else
                 {
-                    valuesSaved = 0;
-                    patternIndex = 0;
+                    endPtr = buffer;
+                    return false;
                 }
                 break;
             case PatternMatchMode::INTEGER:
@@ -119,24 +178,28 @@ int ExtractIntegersFromNextPattern(const char* buffer, const int bufferSize,cons
                     if (patternIndex == patternSize)
                     {
                         endPtr = &buffer[i + 1];
-                        return valuesSaved;
+                        return true;
                     }
                     mode = ExtractMode(pattern, patternSize, patternIndex);
 
+                    if (valuesSaved == maxValues)
+                    {
+                        endPtr = buffer;
+                        return false;
+                    }
                     values[valuesSaved] = (int)val;
                     ++valuesSaved;
                     i += (int)digitsRead - 1; // want to compensate for the extra number of characters read
                 }
                 else
                 {
-                    valuesSaved = 0;
-                    patternIndex = 0; // reset failed to match pattern
-                    mode = ExtractMode(pattern, patternSize, patternIndex);
+                    endPtr = buffer;
+                    return false;
                 }
                 break;
         }
     }
 
-    endPtr = &buffer[bufferSize];
-    return 0;
+    endPtr = buffer;
+    return false;
 }
